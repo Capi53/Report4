7b2be91069aa02c8149fec3209086821df4e5a73
Ryu CAPISTRANO <e185753@ie.u-ryukyu.ac.jp>
2018-11-19 13:49:22 +0900

    report4_version1

--- src/Main/main.java
+++ src/Main/main.java
@@ -2,7 +2,23 @@ package jp.ac.uryukyu.ie.e185753;
 
 public class main {
     public static void main(String[] args) {
-        String str = null;
-        System.out.println(str.length());
+        String str ;
+        int result = len(null);
+        System.out.println("戻り値 = " + result);
+
+    }
+
+    public static int len(String str) {
+        try {
+            int strlen = str.length();
+            return strlen;
+        }catch (NullPointerException N){
+            System.out.println("例外が発生しました。");
+            //System.out.println(N);
+            System.err.println(N.getMessage());
+            return 0;
+
+        }
     }
 }
+
